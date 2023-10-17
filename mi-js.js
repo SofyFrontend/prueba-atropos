57e4729958901f063a3a5915e804069a02af1d22
Sofia Acevedo <sofiacandelaria1992@gmail.com>
2023-10-17 13:20:47 +0100

    añadir archivos

---
+++ mi-js.js
@@ -0,0 +1,14 @@
+const myAtropos = Atropos({
+    el: '.my-atropos',
+    activeOffset: 40,
+    shadowScale: 1.05,
+    onEnter() {
+      console.log('Enter');
+    },
+    onLeave() {
+      console.log('Leave');
+    },
+    onRotate(x, y) {
+      console.log('Rotate', x, y);
+    }
+  });
